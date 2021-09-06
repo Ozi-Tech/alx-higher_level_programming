c1ea1e1c60f9349d1c68298064e5c7fd64f14992
Ekene Ndubueze <ekene.ndubueze@gmail.com>
2021-09-06 19:36:46 +0100

    Fix errors

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 str = """The Zen of Python, by Tim Peters
 
 Beautiful is better than ugly.
@@ -18,5 +19,9 @@ Now is better than never.
 Although never is often better than *right* now.
 If the implementation is hard to explain, it's a bad idea.
 If the implementation is easy to explain, it may be a good idea.
-Namespaces are one honking great idea -- let's do more of those!"""
-print(len(str))
+Namespaces are one honking great idea -- let's do more of those!
+"""
+
+limit = 95
+zen = str[:limit] + '...' * (len(str) > limit)
+print(zen)
